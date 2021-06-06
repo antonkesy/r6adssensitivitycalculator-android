d2bfe4bfbb397891848c739ba2b5ea1ccd487747
Anton Kesy <anton@kesy.de>
2021-06-06 11:42:30 +0200

    Move and merge UserPreferences

--- r6adssensitivitycalculator/app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/settings/UserPreferencesManager.java
+++ r6adssensitivitycalculator/app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/settings/UserPreferencesManager.java
@@ -1,13 +1,19 @@
-package com.poorskill.r6adssensitivitycalculator.utility;
+package com.poorskill.r6adssensitivitycalculator.ui.settings;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.preference.PreferenceManager;
+
+import com.poorskill.r6adssensitivitycalculator.R;
+
+import java.util.Locale;
 
 public class UserPreferencesManager {
     private static final String prefADSKey = "adsKey";
-    private static final String prefFOVKey = "fovKey";
-    private static final String prefPosAspectRatioKey = "aspectPosKey";
+    private static final String prefFOVKey = "fKey";
+    private static final String prefPosAspectRatioKey = "aKey";
     private static final String prefUsageKey = "uKey";
 
     private static SharedPreferences getSharedPreferences(Context context) {
@@ -56,6 +62,39 @@ public class UserPreferencesManager {
 
     public static int getUsage(Context context) {
         return getSharedPreferences(context).getInt(prefUsageKey, 0);
+    }
+
+    public static Theme getApplicationTheme(Context context) {
+        int id;
+        try {
+            id = Integer.parseInt(getSharedPreferences(context).getString(context.getString(R.string.prefApplicationThemePrefKey), "0"));
+        } catch (Exception e) {
+            id = 0;
+        }
+        for (Theme t : Theme.values()) {
+            if (t.getId() == id) {
+                return t;
+            }
+        }
+        return Theme.System;
+    }
+
+    public static void updateLanguage(Context context) {
+        String langCode = getSharedPreferences(context).getString(context.getString(R.string.prefApplicationLanguagePrefKey), "system");
+        if (langCode.equals("system")) {
+            langCode = Locale.getDefault().getLanguage();
+        }
+        setLocale(context, langCode);
+    }
+
+    private static void setLocale(Context context, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Resources r = context.getResources();
+        Configuration c = r.getConfiguration();
+        c.setLocale(locale);
+        r.updateConfiguration(c, r.getDisplayMetrics());
+        context.createConfigurationContext(c);
     }
 
 
