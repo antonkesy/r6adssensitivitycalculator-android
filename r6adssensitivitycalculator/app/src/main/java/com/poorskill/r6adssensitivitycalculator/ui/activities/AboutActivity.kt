f6fa3f6d97315ae3164a86d2df43e6b92e4e013b
Anton Kesy <antonkesy@gmail.com>
2023-07-24 09:18:38 +0200

    create binding help function

--- r6adssensitivitycalculator/app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/activities/AboutActivity.kt
+++ r6adssensitivitycalculator/app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/activities/AboutActivity.kt
@@ -2,6 +2,7 @@ package com.poorskill.r6adssensitivitycalculator.ui.activities
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import com.poorskill.r6adssensitivitycalculator.R
 import com.poorskill.r6adssensitivitycalculator.databinding.ActivityAboutBinding
 import com.poorskill.r6adssensitivitycalculator.ui.copyToClipboard
@@ -20,51 +21,44 @@ class AboutActivity : BaseActivity() {
     binding.versionCodeAbout.text =
         (this.packageManager.getPackageInfo(packageName, 0).versionName).toString()
 
-    binding.websiteButton.setOnClickListener {
+    bindButton(binding.websiteButton, R.string.poorskillWebsite) {
       openURLInBrowser(this.getString(R.string.poorskillWebsite), this)
     }
-    binding.websiteButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.poorskillWebsite), this)
-      return@setOnLongClickListener true
-    }
-    binding.privacyPolicyButton.setOnClickListener {
+
+    bindButton(binding.privacyPolicyButton, R.string.privacyPolicyURL) {
       openURLInBrowser(this.getString(R.string.privacyPolicyURL), this)
     }
-    binding.privacyPolicyButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.privacyPolicyURL), this)
-      return@setOnLongClickListener true
-    }
-    binding.contactButton.setOnClickListener {
+
+    bindButton(binding.contactButton, R.string.contactMail) {
       openMail(this.getString(R.string.contactMail), this.getString(R.string.app_name), this)
     }
-    binding.contactButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.contactMail), this)
-      return@setOnLongClickListener true
-    }
-    binding.sourceCodeButton.setOnClickListener {
+
+    bindButton(binding.sourceCodeButton, R.string.sourceCodeURL) {
       openURLInBrowser(this.getString(R.string.sourceCodeURL), this)
     }
-    binding.sourceCodeButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.sourceCodeURL), this)
-      return@setOnLongClickListener true
-    }
-    binding.reportBugButton.setOnClickListener {
+
+    bindButton(binding.reportBugButton, R.string.reportBugURL) {
       openURLInBrowser(this.getString(R.string.reportBugURL), this)
     }
-    binding.reportBugButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.reportBugURL), this)
-      return@setOnLongClickListener true
-    }
-    binding.rateAppButton.setOnClickListener {
+
+    bindButton(binding.rateAppButton, R.string.rateAppURL) {
       openURLInBrowser(this.getString(R.string.rateAppURL), this)
-    }
-    binding.rateAppButton.setOnLongClickListener {
-      copyToClipboard(this.getString(R.string.rateAppURL), this)
-      return@setOnLongClickListener true
     }
 
     setTitle(R.string.aboutTitle)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
+  private fun bindButton(
+      btn: View,
+      longPressCopyStringRes: Int,
+      onClick: () -> Unit,
+  ) {
+    btn.setOnClickListener { onClick() }
+    btn.setOnLongClickListener {
+      copyToClipboard(this.getString(longPressCopyStringRes), this)
+      return@setOnLongClickListener true
+    }
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
