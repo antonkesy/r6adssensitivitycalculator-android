f88448b1bf73689a72859c137f927e457fe1f78a
Anton Kesy <anton@kesy.de>
2026-07-22 13:22:55 +0200

    fix overlapping ActionBar

--- app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/activities/BaseActivity.kt
+++ app/src/main/java/com/poorskill/r6adssensitivitycalculator/ui/activities/BaseActivity.kt
@@ -3,6 +3,9 @@ package com.poorskill.r6adssensitivitycalculator.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.poorskill.r6adssensitivitycalculator.R
 import com.poorskill.r6adssensitivitycalculator.settings.Settings
 import com.poorskill.r6adssensitivitycalculator.settings.UserPreferencesManager
@@ -12,6 +15,7 @@ open class BaseActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    applyEdgeToEdgeInsets()
     val settings: Settings = UserPreferencesManager(this)
     settings.updateLanguage()
     when (val themePref = settings.getTheme()) {
@@ -32,6 +36,18 @@ open class BaseActivity : AppCompatActivity() {
             }
         )
       }
+    }
+  }
+
+  /** Pads the window for the status/nav bars and display cutout, since targetSdk 35+ draws edge-to-edge by default. */
+  private fun applyEdgeToEdgeInsets() {
+    ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+      val bars =
+          insets.getInsets(
+              WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+          )
+      view.updatePadding(left = bars.left, top = bars.top, right = bars.right, bottom = bars.bottom)
+      WindowInsetsCompat.CONSUMED
     }
   }
 }
